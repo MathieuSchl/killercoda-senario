01c40f4605574800741facb84180e2dbca8d5854
CodySen <mathieuman78@gmail.com>
2023-11-16 17:32:47 +0100

    Add bashgame

---
+++ bash-game/foreground.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+useradd gameshell
+cd /tmp
+sudo apt install gettext man-db procps psmisc nano tree bsdmainutils x11-apps wget -y
+wget https://github.com/phyver/GameShell/releases/download/latest/gameshell.sh
+runuser -l gameshell -c 'bash gameshell.sh'
---
+++ bash-game/intro.md
@@ -0,0 +1,5 @@
+
+<br>
+
+Have fun
+
