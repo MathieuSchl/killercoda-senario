51ac98dfda32f9600797752b261f3a43326c80db
CodySen <58334020+MathieuSchl@users.noreply.github.com>
2023-11-17 10:29:47 +0100

    Update foreground.sh

--- bash-game/foreground.sh
+++ bash-game/foreground.sh
@@ -4,6 +4,6 @@ echo 'useradd gameshell' > /tmp/script_install.sh
 echo 'cd /tmp' > /tmp/script_install.sh
 echo 'apt install gettext man-db procps psmisc nano tree bsdmainutils x11-apps wget -y' > /tmp/script_install.sh
 echo 'wget https://github.com/phyver/GameShell/releases/download/latest/gameshell.sh' > /tmp/script_install.sh
-echo 'runuser -l gameshell -c \'bash gameshell.sh\'' > /tmp/script_install.sh
+echo 'runuser -l gameshell -c "bash gameshell.sh"' > /tmp/script_install.sh
 
 sh /tmp/script_install.sh
