c975e859d438f9b0c0297661dff83ceeeec5fcbd
CodySen <58334020+MathieuSchl@users.noreply.github.com>
2023-11-17 10:27:29 +0100

    Create installation script

--- bash-game/foreground.sh
+++ bash-game/foreground.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+echo '#!/bin/bash' > /tmp/script_install.sh
 
-useradd gameshell
-cd /tmp
-apt install gettext man-db procps psmisc nano tree bsdmainutils x11-apps wget -y
-wget https://github.com/phyver/GameShell/releases/download/latest/gameshell.sh
-runuser -l gameshell -c 'bash gameshell.sh'
+echo 'useradd gameshell' > /tmp/script_install.sh
+echo 'cd /tmp' > /tmp/script_install.sh
+echo 'apt install gettext man-db procps psmisc nano tree bsdmainutils x11-apps wget -y' > /tmp/script_install.sh
+echo 'wget https://github.com/phyver/GameShell/releases/download/latest/gameshell.sh' > /tmp/script_install.sh
+echo 'runuser -l gameshell -c \'bash gameshell.sh\'' > /tmp/script_install.sh
+
+sh /tmp/script_install.sh
