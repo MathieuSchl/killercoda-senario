84d3bd5176ffec0c0f0fa80d428cb9093bc3a6cc
CodySen <58334020+MathieuSchl@users.noreply.github.com>
2023-11-17 10:20:52 +0100

    Remove sudo

--- bash-game/foreground.sh
+++ bash-game/foreground.sh
@@ -2,6 +2,6 @@
 
 useradd gameshell
 cd /tmp
-sudo apt install gettext man-db procps psmisc nano tree bsdmainutils x11-apps wget -y
+apt install gettext man-db procps psmisc nano tree bsdmainutils x11-apps wget -y
 wget https://github.com/phyver/GameShell/releases/download/latest/gameshell.sh
 runuser -l gameshell -c 'bash gameshell.sh'
