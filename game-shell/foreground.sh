eac4949792fe45d3253ed926d2fb352198511a0c
CodySen <mathieuman78@gmail.com>
2023-11-20 09:45:22 +0100

    Remove issue with commentary

--- game-shell/foreground.sh
+++ game-shell/foreground.sh
@@ -3,18 +3,16 @@ echo 'echo "GameShell starting"' >> /tmp/run_game.sh
 echo 'echo "Please wait"' >> /tmp/run_game.sh
 echo '' >> /tmp/run_game.sh
 echo 'while true; do' >> /tmp/run_game.sh
-echo '    # Vérifie si le fichier existe' >> /tmp/run_game.sh
 echo '    if [ -e "/tmp/gameshell.sh" ]; then' >> /tmp/run_game.sh
 echo '        break' >> /tmp/run_game.sh
 echo '    else' >> /tmp/run_game.sh
 echo '        echo -n "."' >> /tmp/run_game.sh
 echo '    fi' >> /tmp/run_game.sh
 echo '' >> /tmp/run_game.sh
-echo '    \# Attend une seconde avant de vérifier à nouveau' >> /tmp/run_game.sh
 echo '    sleep 1' >> /tmp/run_game.sh
 echo 'done' >> /tmp/run_game.sh
 
-#echo 'runuser -l gameshell -c "bash /tmp/gameshell.sh"' >> /tmp/run_game.sh
+echo 'runuser -l gameshell -c "bash /tmp/gameshell.sh"' >> /tmp/run_game.sh
 
 cd /tmp
 sh /tmp/run_game.sh
